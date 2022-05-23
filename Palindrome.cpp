9c62c9c2ba04f26f0b8a3a4b897dae5ac294cd20
Youssef-Nassar <89210206+Youssef-Nassar@users.noreply.github.com>
2022-05-23 07:55:55 +0200

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -23,29 +23,29 @@ int main(){
     Queue q;
     
     for(int i = 0; i < size; i++){
-    		// We converted all characters into smaller case characters.
-    		// To make the camporision valid for all test cases.
-    		char c;
-    		c = tolower(line[i]);
-    		// Here we only inserted characters or numbers
-    		// in the the stack and queue to avoid adding spaces or any special characters
-    		// To reach an optimal solution for all test cases.
-    		if(c>='a' && c <='z' || c >='0' && c<='9'){
+    	// We converted all characters into smaller case characters.
+    	// To make the camporision valid for all test cases.
+    	char c;
+    	c = tolower(line[i]);
+    	// Here we only inserted characters or numbers
+    	// in the the stack and queue to avoid adding spaces or any special characters
+    	// To reach an optimal solution for all test cases.
+    	if(c>='a' && c <='z' || c >='0' && c<='9'){
         		// The counter to know the length of the line without special characters.
         		counter++;
         		stack.push(c);
         		q.enQueue(c);
     		}
   
-		}
+	}
 
-		int x =0;
-		for(int i = 0; i < counter; i++){
+	int x =0;
+	for(int i = 0; i < counter; i++){
     		// x to know then numbers of characters that have checked the condition.
     		if(stack.pop() == q.dequeue()) x++;
-		}
+	}
 
-		if(x == counter) cout<<"Palindrome";
-		else cout<<"Not Palindrome";
-		return 0;
+	if(x == counter) cout<<"Palindrome";
+	else cout<<"Not Palindrome";
+	return 0;
 }
